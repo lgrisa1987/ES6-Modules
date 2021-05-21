bdb056a1244800ed75b3da10c27d621492794611
Leandro Grisalena Morello <leagrisa@WKMZT0DD141F.global.publicisgroupe.net>
2021-05-21 17:43:58 -0300

    Exporting and Importing in ES6 Modules

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 //Importing Module
-import './shoppingCart.js';
+/* import {
+    addToCart,
+    totalPrice as price,
+    tq
+} from './shoppingCart.js';
+addToCart("breads", 5);
+console.log(price, tq); */
 console.log('Importing module');
+
+/* import * as ShoppingCart from './shoppingCart.js';
+ShoppingCart.addToCart("breads", 5); */
+
+import add, {
+    cart
+} from "./shoppingCart.js";
+
+add("pizza", 2)
+add("bread", 5)
+add("apples", 4)
+
+console.log(cart);
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,4 +1,25 @@
 //Exporting Module
 console.log('Exporting module');
-const shoppingCost = 11,
-    cart = [];
+const shoppingCost = 11;
+export const cart = [];
+const totalPrice = 237,
+    totalQuantity = 23;
+export const addToCart = (product, quantity) => {
+    cart.push({
+        product,
+        quantity
+    });
+    console.log(`${quantity} ${product} were added to the cart`);
+}
+export {
+    totalPrice,
+    totalQuantity as tq
+};
+
+export default (product, quantity) => {
+    cart.push({
+        product,
+        quantity
+    })
+    console.log(`${quantity} ${product} were added to the cart`);
+}
