42438f143c0a051a783c11502e268964e5ae1c7d
Leandro <leagrisalena@gmail.com>
2021-05-26 01:12:55 -0300

    CommonJS Modules

--- script.js
+++ script.js
@@ -6,12 +6,12 @@
 } from './shoppingCart.js';
 addToCart("breads", 5);
 console.log(price, tq); */
-console.log('Importing module');
+/* console.log('Importing module'); */
 
 /* import * as ShoppingCart from './shoppingCart.js';
 ShoppingCart.addToCart("breads", 5); */
 
-import add, {
+/* import add, {
     cart
 } from "./shoppingCart.js";
 
@@ -20,3 +20,44 @@ add("bread", 5)
 add("apples", 4)
 
 console.log(cart);
+
+const shoppingCart2 = (function () {
+    const cart = [],
+        shippingCost = 10,
+        totalPrice = 237,
+        totalQuantity = 23,
+        addToCart = (product, quantity) => {
+            cart.push({
+                product,
+                quantity
+            });
+            console.log(`${quantity} ${product} were added to the cart, shipping cost is ${shippingCost}`);
+        },
+        orderStock = (product, quantity) => console.log(`${quantity} ${product} orderer from supplier`);
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity
+    }
+})();
+
+shoppingCart2.addToCart('apple', 4);
+shoppingCart2.addToCart('pizza', 2);
+
+console.log(shoppingCart2);
+console.log(shoppingCart2.shippingCost); */
+
+//Export 
+export.addToCart = (product, quantity) => {
+    cart.push({
+        product,
+        quantity
+    });
+    console.log(`${quantity} ${product} were added to the cart, shipping cost is ${shippingCost}`);
+}
+
+//Import
+const {
+    addToCart
+} = require("./shoppingCart.js")
